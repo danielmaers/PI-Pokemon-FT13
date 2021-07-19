6c38117f15f326772812519bcf33e92e5f3f18bf
Daniel <daniel.maers@gmail.com>
2021-07-19 13:39:09 -0300

    deploy

--- api/src/app.js
+++ api/src/app.js
@@ -11,28 +11,28 @@ const server = express();
 
 server.name = "API";
 
-//server.use(cors());
-server.use(
-  cors({
-    origin: "*",
-    credentials: true,
-    methods: ["GET", "POST", "OPTIONS", "PUT", "DELETE"],
-    allowedHeaders: [
-      "Origin",
-      "X-Requested-With",
-      "Content-Type",
-      "Accept",
-      "authorization",
-    ],
-  })
-);
+server.use(cors());
+// server.use(
+//   cors({
+//     origin: "*",
+//     credentials: true,
+//     methods: ["GET", "POST", "OPTIONS", "PUT", "DELETE"],
+//     allowedHeaders: [
+//       "Origin",
+//       "X-Requested-With",
+//       "Content-Type",
+//       "Accept",
+//       "authorization",
+//     ],
+//   })
+// );
 
 server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 server.use(bodyParser.json({ limit: "50mb" }));
 server.use(cookieParser());
 server.use(morgan("dev"));
 server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
